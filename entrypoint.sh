0abd58b1302a0753db4abbe9f9a8e9afb7c6e908
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-07-15 18:31:19 -0300

    Edited entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ export PIP_EXTRA_INDEX_URL="https://pypi.org/simple/"
 
 echo ${NODIS_REGISTRY_PASSWORD} | docker login ${NODIS_REGISTRY_HOST} -u ${NODIS_REGISTRY_USER} --password-stdin
 
-if [[ ${GITHUB_REF} == "ref/head/master" && ${NODIS_SERVICE_TYPE} != "cronjob" ]]; then
+if [[ ${GITHUB_REF} == "ref/head/master" && ${NODIS_SERVICE_TYPE} == "deployment" ]]; then
 
     docker pull ${NODIS_IMAGE_NAME}:${NODIS_BASE_VERSION}
     docker tag ${NODIS_IMAGE_NAME}:${NODIS_BASE_VERSION} ${NODIS_IMAGE_NAME}:${NODIS_DEPLOY_ENV}
@@ -15,14 +15,11 @@ else
 
     docker build . -t ${NODIS_IMAGE_NAME} --build-arg PIP_INDEX_URL --build-arg PIP_EXTRA_INDEX_URL
 
-    docker tag ${NODIS_IMAGE_NAME} ${NODIS_IMAGE_NAME}:${NODIS_FULL_VERSION}
-    docker tag ${NODIS_IMAGE_NAME} ${NODIS_IMAGE_NAME}:${NODIS_DEPLOY_ENV}
-    docker tag ${NODIS_IMAGE_NAME} ${NODIS_IMAGE_NAME}:${NODIS_BASE_VERSION}
-    docker tag ${NODIS_IMAGE_NAME} ${NODIS_IMAGE_NAME}:${NODIS_CUSTOM_TAG}
+    for TAG in ${NODIS_IMAGE_TAGS}; do
 
-    docker push ${NODIS_IMAGE_NAME}:${NODIS_FULL_VERSION}
-    docker push ${NODIS_IMAGE_NAME}:${NODIS_DEPLOY_ENV}
-    docker push ${NODIS_IMAGE_NAME}:${NODIS_BASE_VERSION}
-    docker push ${NODIS_IMAGE_NAME}:${NODIS_CUSTOM_TAG}
+        docker tag ${NODIS_IMAGE_NAME} ${NODIS_IMAGE_NAME}:${TAG}
+        docker push ${NODIS_IMAGE_NAME}:${TAG}
+
+    done
 
 fi
