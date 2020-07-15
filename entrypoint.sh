828f001b37526d1147e16012a26b8981e814224e
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-07-15 18:48:51 -0300

    Edited entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@
 export PIP_INDEX_URL="https://${NODIS_PYPI_USER}:${NODIS_PYPI_PASSWORD}@${NODIS_PYPI_HOST}/simple"
 export PIP_EXTRA_INDEX_URL="https://pypi.org/simple/"
 
-echo ${NODIS_REGISTRY_PASSWORD} | docker login ${NODIS_REGISTRY_HOST} -u ${NODIS_REGISTRY_USER} --password-stdin
+[[ -z ${DOCKER_HUB_USER} ]] || echo ${DOCKER_HUB_PASSWORD} | docker login docker.io -u ${DOCKER_HUB_USER} --password-stdin
+[[ -z ${NODIS_GERISTRY_USER} ]] || echo ${NODIS_REGISTRY_PASSWORD} | docker login ${NODIS_REGISTRY_HOST} -u ${NODIS_REGISTRY_USER} --password-stdin
 
 if [[ ${GITHUB_REF} == "ref/head/master" && ${NODIS_SERVICE_TYPE} == "deployment" ]]; then
 
@@ -14,12 +15,9 @@ if [[ ${GITHUB_REF} == "ref/head/master" && ${NODIS_SERVICE_TYPE} == "deployment
 else
 
     docker build . -t ${NODIS_IMAGE_NAME} --build-arg PIP_INDEX_URL --build-arg PIP_EXTRA_INDEX_URL
-
     for TAG in ${NODIS_IMAGE_TAGS}; do
-
         docker tag ${NODIS_IMAGE_NAME} ${NODIS_IMAGE_NAME}:${TAG}
         docker push ${NODIS_IMAGE_NAME}:${TAG}
-
     done
 
 fi
