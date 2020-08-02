a7fd28fc756bd690fc68fcf2a27c98e6594ccff2
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-08-02 15:20:23 -0300

    Edited entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 export PIP_INDEX_URL="https://${NODIS_PYPI_USER}:${NODIS_PYPI_PASSWORD}@${NODIS_PYPI_HOST}/simple"
 export PIP_EXTRA_INDEX_URL="https://pypi.org/simple/"
@@ -6,7 +6,7 @@ export PIP_EXTRA_INDEX_URL="https://pypi.org/simple/"
 [[ -z ${DOCKER_HUB_USER} ]] || echo ${DOCKER_HUB_PASSWORD} | docker login docker.io -u ${DOCKER_HUB_USER} --password-stdin
 [[ -z ${NODIS_REGISTRY_USER} ]] || echo ${NODIS_REGISTRY_PASSWORD} | docker login ${NODIS_REGISTRY_HOST} -u ${NODIS_REGISTRY_USER} --password-stdin
 
-if [[ ${GITHUB_REF} == "ref/head/master" && ${NODIS_SERVICE_TYPE} == "deployment" ]]; then
+if [[ ${GITHUB_REF} == "ref/head/master" && -z ${NODIS_SERVICE_TYPE} && ${NODIS_SERVICE_TYPE} == "deployment" ]]; then
 
     docker pull ${NODIS_IMAGE_NAME}:${NODIS_BASE_VERSION}
     docker tag ${NODIS_IMAGE_NAME}:${NODIS_BASE_VERSION} ${NODIS_IMAGE_NAME}:${NODIS_DEPLOY_ENV}
