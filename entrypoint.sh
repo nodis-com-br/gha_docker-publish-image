0c389fc298a6888512541190b14237fc1e006e55
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-09-10 21:49:54 -0300

    Edited entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env sh
 
 export PIP_INDEX_URL="https://${NODIS_PYPI_USER}:${NODIS_PYPI_PASSWORD}@${NODIS_PYPI_HOST}/simple"
-export PIP_EXTRA_INDEX_URL="https://pypi.org/simple/"
+
+DOCKER_BUILD_ARGUMENTS="--build-arg PIP_INDEX_URL"
 
 [[ -z ${DOCKER_HUB_USER} ]] || echo ${DOCKER_HUB_PASSWORD} | docker login docker.io -u ${DOCKER_HUB_USER} --password-stdin
 [[ -z ${NODIS_REGISTRY_USER} ]] || echo ${NODIS_REGISTRY_PASSWORD} | docker login ${NODIS_REGISTRY_HOST} -u ${NODIS_REGISTRY_USER} --password-stdin
 
-if [[ ${GITHUB_REF} == "ref/head/master" && -z ${NODIS_SERVICE_TYPE} && ${NODIS_SERVICE_TYPE} == "deployment" ]]; then
+if [[ ${GITHUB_REF} == "ref/head/master" && ${DEPLOY_RC_TO_PROD:-""} == "true" ]]; then
 
-    docker pull ${NODIS_IMAGE_NAME}:${NODIS_BASE_VERSION}
-    docker tag ${NODIS_IMAGE_NAME}:${NODIS_BASE_VERSION} ${NODIS_IMAGE_NAME}:${NODIS_DEPLOY_ENV}
-    docker push ${NODIS_IMAGE_NAME}:${NODIS_DEPLOY_ENV}
+    docker pull ${DOCKER_IMAGE_NAME}:${NODIS_PROJECT_VERSION}
+    docker tag ${DOCKER_IMAGE_NAME}:${NODIS_PROJECT_VERSION} ${DOCKER_IMAGE_NAME}:${NODIS_DEPLOY_ENV}
+    docker push ${DOCKER_IMAGE_NAME}:${NODIS_DEPLOY_ENV}
 
 else
 
-    docker build . -t ${NODIS_IMAGE_NAME} --build-arg PIP_INDEX_URL --build-arg PIP_EXTRA_INDEX_URL
-    for TAG in ${NODIS_IMAGE_TAGS}; do
-        docker tag ${NODIS_IMAGE_NAME} ${NODIS_IMAGE_NAME}:${TAG}
-        docker push ${NODIS_IMAGE_NAME}:${TAG}
+    docker build . -t ${DOCKER_IMAGE_NAME} ${DOCKER_BUILD_ARGUMENTS}
+    for TAG in ${DOCKER_IMAGE_TAGS}; do
+        docker tag ${DOCKER_IMAGE_NAME} ${DOCKER_IMAGE_NAME}:${TAG}
+        docker push ${DOCKER_IMAGE_NAME}:${TAG}
     done
 
 fi
