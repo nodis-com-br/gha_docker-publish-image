f253edde5391f7421c4a6c2c5b890a6cf404af36
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-11-17 15:26:41 -0300

    Edited entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env sh
 set -e
 
-echo "#################################################################"
-#cat ${GITHUB_ENV}
-#echo ${GITHUB_ENV}
-head -n 100 /github/file_commands/set_env*
-
-echo "##################################################################"
-
 env | sort
 
 DOCKER_BUILD_ARGUMENTS="--build-arg PIP_INDEX_URL=https://${NODIS_PYPI_USER}:${NODIS_PYPI_PASSWORD}@${NODIS_PYPI_HOST}/simple"
